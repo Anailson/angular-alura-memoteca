c1be9c8d892b959215edf880d2c9292bacac7cdc
anailson ribeiro <arsantos315@gmail.com>
2022-10-29 10:57:27 -0300

    comunicação entre os componentes

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = []
+  listaPensamentos = [
+    {
+      conteudo:'Passo informaões para o componente filho',
+      autoria:'Componente Pai',
+      modelo:'modelo3'
+    },
+    {
+      conteudo:'Minha propriedade é decorada com @Input()',
+      autoria:'Compoennete filho',
+      modelo:'modelo2'
+    }
+  ];
 
   constructor() { }
 
